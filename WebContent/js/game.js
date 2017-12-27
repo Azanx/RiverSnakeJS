097a7017ab649e6f81417a90749c41f5663ad1b0
Azanx <2915102+Azanx@users.noreply.github.com>
2017-12-27 19:57:17 +0100

    createFood method now authomatically assigns new object to food variable

--- WebContent/js/game.js
+++ WebContent/js/game.js
@@ -4,17 +4,19 @@
 
 
 var Game = (function() {
-	var food = new CreateFood();
+	var food;// = new CreateFood();
 	document.getElementById('score').textContent = score;
 	document.getElementById('highscore').textContent = highscore;
 
-	function CreateFood() {
+	function createFood() {
 		this.x = Math.floor(Math.random() * (mapWidth / fieldSize));
 		this.y = Math.floor(Math.random() * (mapHeight / fieldSize));
-		return this;
+		food = this;
 	}
-
 	snake.move();
+	createFood();
+	alert('Food x: ' + food.x + ', y: ' + food.y);
+	createFood();
 	alert('Food x: ' + food.x + ', y: ' + food.y);
 	
 	return {
