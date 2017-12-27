59cae85833aeb8ac2fda32bfd045cbbdb203dd07
Azanx <2915102+Azanx@users.noreply.github.com>
2017-12-27 21:05:51 +0100

    add drawing snake

--- WebContent/js/draw.js
+++ WebContent/js/draw.js
@@ -6,8 +6,16 @@ var Draw = (function() {
 	var bgColor = 'navy';
 	var snakeColor = 'green';
 	var snakeHeadColor = 'darkgreen';
+	var snakePartBorder = 'black';
 	var foodColor = 'yellow';
 
+	
+	var drawFullMap = function(food, snakeBody) {
+		_fillMapBackground();
+		_drawFood(food);
+		_drawSnake(snakeBody);
+	}
+	
 	function _fillMapBackground() {
 		ctx.fillStyle=bgColor;
 		ctx.fillRect(0, 0, mapWidth, mapHeight);
@@ -17,15 +25,27 @@ var Draw = (function() {
 		var radius = fieldSize/2;
 		var xPos = food.x*fieldSize+radius;
 		var yPos = food.y*fieldSize+radius;
+		ctx.beginPath();
 		ctx.arc(xPos, yPos, fieldSize/2, radius, 0, 2*Math.PI);
 		ctx.fillStyle = foodColor;
 		ctx.fill();
 	}
 	
-	var drawFullMap = function(food) {
-		_fillMapBackground();
-		_drawFood(food);
-	};
+	function _drawSnake(snakeBody) {
+		_drawSnakePart(snakeBody[0], snakeHeadColor);
+		for(var i=1; i<snakeBody.length; i++) {
+			_drawSnakePart(snakeBody[i], snakeColor);
+		}
+	}
+	
+	function _drawSnakePart(part, color) {
+		ctx.fillStyle = color;
+		ctx.fillRect(part.x * fieldSize, part.y * fieldSize, fieldSize, fieldSize);
+		
+		ctx.strokeStyle=snakePartBorder;
+		ctx.lineWidth = 2;
+		ctx.strokeRect(part.x * fieldSize, part.y * fieldSize, fieldSize, fieldSize);
+	}
 	
 	return {
 		drawFullMap : drawFullMap
