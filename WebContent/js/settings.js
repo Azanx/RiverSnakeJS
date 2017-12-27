44c909de073e2413b9589c43e190736fb9886b51
Azanx <2915102+Azanx@users.noreply.github.com>
2017-12-27 21:25:41 +0100

    add comment for foodValue

--- WebContent/js/settings.js
+++ WebContent/js/settings.js
@@ -8,7 +8,7 @@ var mapHeight = canvas.height;
 var fieldSize = 20; // size of one map field, also size of all objects on
 // the map
 var score = 0;
-var foodValue = 10;
+var foodValue = 10; //point value of food for generating score
 var highscore = 0;
 var isActive = false;// is game in active state or otherwise not started
 // movement direction, x and y fields mean how many fields to move in which
